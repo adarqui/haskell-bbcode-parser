402f59c02dfd0117f385281dcb09f625a0b108c3
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-13 04:45:20 -0400

    follow the pattern, i guess

--- test/Data/BBCode/TypesSpec.hs
+++ test/Data/BBCode/TypesSpec.hs
@@ -26,7 +26,8 @@ spec = do
   describe "flattenTokens" $ do
     it "flattens tokens into a string" $ do
 
-      flattenTokens (Cons (BBStr "hi") Nil) `shouldBe` "BBStr \"hi\""
+      flattenTokens (Cons (BBStr "hi") Nil)
+        `shouldBe` "BBStr \"hi\""
 
       flattenTokens (Cons (BBOpen Nothing "b") (Cons (BBStr "hi") (Cons (BBClosed "b") Nil)))
         `shouldBe` "BBOpen Nothing \"b\",BBStr \"hi\",BBClosed \"b\""
